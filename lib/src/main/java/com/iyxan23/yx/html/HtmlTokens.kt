a5a3869e0bcecafc2b595466882908fe5d47f43b
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-30 15:33:21 +0700

    feat: Partially implement lexical analysis

---
+++ lib/src/main/java/com/iyxan23/yx/html/HtmlTokens.kt
@@ -0,0 +1,12 @@
+package com.iyxan23.yx.html
+
+enum class HtmlTokens {
+    OPEN_TAG,         // <
+    TAG_NAME,         // <{tag name}
+    ATTRIBUTE_NAME,   //  {tag name} {attribute name}=
+    ATTRIBUTE_VALUE,  //             {attribute name}={attribute_value}
+    CLOSE_TAG,        //                                               >
+    TEXT_VALUE,       //                                               >{text value}
+    NO_CHILD_CLOSE,   //                                               />
+    OPEN_CLOSING_TAG, // </
+}
