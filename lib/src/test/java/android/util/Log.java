59cc6fb30bd7d1006efadee4fbd4c42540501774
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-14 16:49:47 +0700

    test: add mock Log

---
+++ lib/src/test/java/android/util/Log.java
@@ -0,0 +1,26 @@
+package android.util;
+
+/**
+ * Mock android.util.Log so we can get the log output during unit testing a component that uses Log
+ */
+public class Log {
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+}
